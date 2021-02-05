963d7777fdc0a3b6fa03a4cbae18e6ffea7cf479
Akribes <30956441+Akribes@users.noreply.github.com>
2021-02-05 23:13:28 +0100

    Finish forward propagation

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 import math
 
 weights = [
-        [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0] # bias
-        ],
-        [
-            [0],
-            [0],
-            [0],
-            [0] # bias
-        ]
+    [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0]
     ]
+]
 
 activation = [
-        [],
-        []
-    ]
+    [],
+    []
+]
 
 derivatives = [
-        [],
-        []
-    ]
+    [],
+    []
+]
 
 loss = 0
+
 
 def C(y, x):
     return y - x
@@ -33,46 +31,66 @@ def C(y, x):
 def f(x):
     return 1 / (1 + math.exp(-x))
 
+
 def f_deriv(x):
     return f(x) * f(-x)
+
 
 def f_vector(x):
     res = []
     for a in x:
-        res.append(f(a))
+        res.append(f(a[0]))
     return res
+
 
 def f_deriv_vector(x):
     res = []
     for a in x:
-        res.append(f_deriv(a))
+        res.append(f_deriv(a[0]))
     return res
 
 
 def multiply_matrices(A, B):
+    print("Multiplying", A, "and", B)
     if len(A[0]) != len(B):
         raise ValueError("Matrix sizes do not match")
-    res = [[0] * len(B[0])] * len(A)
+    res = [[0] * len(B[0]) for i in range(len(A))]
 
-    for row in range(len(res)):
-        for cell in range(len(row)):
-            for i in range(len(res)):
-                pass # TODO actually implement this
-            
+    for j in range(len(res)):
+        for i in range(len(res[j])):
+            for k in range(len(B)):
+                res[j][i] += A[j][k] * B[k][i]
     return res
+
+
+def transpose_matrix(A):
+    print("Transposing", A)
+    AT = [[0] * len(A) for i in range(len(A[0]))]
+
+    for j in range(len(A)):
+        for i in range(len(A[j])):
+            AT[i][j] = A[j][i]
+
+    return AT
 
 
 def propagate(x):
     for i in range(len(weights)):
-        z = None
+        o = None
         if i == 0:
-            z = x
+            o = x[:]
+            print(o)
         else:
-            z = multiply_matrix(weights[i], activation[i - 1])
+            o = activation[i - 1]
+        o.append(1)  # bias
+
+        z = transpose_matrix([o])
+        z = multiply_matrices(weights[i], z)
+        print("Weighted inputs", z)
         activation[i] = f_vector(z)
         derivatives[i] = f_deriv_vector(z)
 
-    return activation[-1] # return output
+    return activation[-1]  # return output
 
 
-print(multiply_matrices([[0,0,0],[0,0,0]], [[0,0],[0,0],[0,0]]))
+print(propagate([1, 2]))
